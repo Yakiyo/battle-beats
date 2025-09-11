ea0380b3a004f91eddd9035d50c0034bb5c61109
Yakiyo <yakiyo1206@gmail.com>
2025-09-11 22:53:09 +0600

    chore: impromptu commit

--- main.c
+++ main.c
@@ -9,16 +9,12 @@
 #include "raylib.h"
 #include "util.h"
 #include "beat.h"
+#include "page.h"
 
-PageType currentPage = PAGE_OPENING;
-Texture2D charTexture1;
 char* beatFile = NULL;
 Beatmap* currentBeatmap = NULL;
 Music music;
 
-float _fader_timer = 0.0f;
-
-void drawOpeningPage(int is_menu);
 
 void drawBeatSelectionPage(int is_multi);
 
@@ -34,27 +30,8 @@ int main() {
   InitWindow(screenWidth, screenHeight, "Beat Game");
   InitAudioDevice();
   SetTargetFPS(60);
+  loadAssets();
 
-  Image charImage1 = LoadImage("asset/guiter-player-1.png");
-  Image a_right_i = LoadImage("asset/beat-right.png");
-  Image a_left_i = LoadImage("asset/beat-left.png");
-  Image a_up_i = LoadImage("asset/beat-up.png");
-  Image a_down_i = LoadImage("asset/beat-down.png");
-
-  // 100x100 images (50px radius)
-  ImageResize(&a_right_i, 100, 100);
-  ImageResize(&a_left_i, 100, 100);
-  ImageResize(&a_up_i, 100, 100);
-  ImageResize(&a_down_i, 100, 100);
-
-  charTexture1 = LoadTextureFromImage(charImage1);
-
-  // unload images from RAM after loading textures
-  UnloadImage(charImage1);
-  UnloadImage(a_right_i);
-  UnloadImage(a_left_i);
-  UnloadImage(a_up_i);
-  UnloadImage(a_down_i);
   // Global GUI styles
   GuiSetStyle(DEFAULT, TEXT_SIZE, 50);
   GuiSetStyle(DEFAULT, TEXT_SPACING, 2);
@@ -80,14 +57,14 @@ int main() {
     ClearBackground(BLACK);
 
     int quit = 0;
-    switch (currentPage) {
+    switch (getCurrentPage()) {
       case PAGE_OPENING:
       case PAGE_MENU:
-        drawOpeningPage(currentPage == PAGE_MENU ? 1 : 0);
+        drawOpeningPage(getCurrentPage() == PAGE_MENU ? 1 : 0);
         break;
       case BEAT_SELECTION_SINGLE:
       case BEAT_SELECTION_MULTI:
-        drawBeatSelectionPage(currentPage == BEAT_SELECTION_MULTI ? 1 : 0);
+        drawBeatSelectionPage(getCurrentPage() == BEAT_SELECTION_MULTI ? 1 : 0);
         break;
       case PAGE_SINGLEPLAYER:
         drawSingleplayerPage();
@@ -100,7 +77,7 @@ int main() {
     if (quit) break;
   }
 
-  UnloadTexture(charTexture1);
+  unloadAssets();
   unloadBeatmaps();
   CloseAudioDevice();
   CloseWindow();
@@ -110,65 +87,6 @@ int main() {
 /////////////////////////////////////////////////////
 /////////////////////////////////////////////////////
 /////////////////////////////////////////////////////
-
-void drawOpeningPage(int is_menu) {
-  char* title = "Battle Beats";
-  int titleWidth = MeasureText(title, titleFontSize);
-
-  DrawText(title, (screenWidth - titleWidth) / 2, screenHeight / 4,
-           titleFontSize, PINK);
-
-  if (!is_menu) {
-    // change alpha over time which makes the text fade in and out
-    _fader_timer += GetFrameTime();
-    float alpha =
-        (sinf(_fader_timer * 2.0f) + 1.0f) * 0.5f * 255;  // 2.0f is speed
-
-    char* text = "Press ENTER to continue";
-    DrawText(text, (screenWidth - MeasureText(text, textFontSize)) / 2,
-             // light gray color
-             screenHeight / 2, textFontSize, (Color){200, 200, 200, alpha});
-
-    if (IsKeyPressed(KEY_ENTER) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-      currentPage = PAGE_MENU;
-      _fader_timer = 0.0f;  // reset timer for next use
-    }
-  } else {
-    int buttonWidth = 500;
-    int buttonHeight = 100;
-    int b_singleplayer =
-        GuiButton((Rectangle){(screenWidth - buttonWidth) / 2,
-                              screenHeight / 2 - 60, buttonWidth, buttonHeight},
-                  "Singleplayer");
-
-    int b_multiplayer = GuiButton(
-        (Rectangle){(screenWidth - buttonWidth) / 2,
-                    screenHeight / 2 - 60 + 100, buttonWidth, buttonHeight},
-        "Multiplayer");
-    int b_generator = GuiButton(
-        (Rectangle){(screenWidth - buttonWidth) / 2,
-                    screenHeight / 2 - 60 + 200, buttonWidth, buttonHeight},
-        "Beatmap Generator");
-
-    int b_quit = GuiButton(
-        (Rectangle){(screenWidth - buttonWidth) / 2,
-                    screenHeight / 2 - 60 + 300, buttonWidth, buttonHeight},
-        "Quit Game");
-
-    DrawTexture(charTexture1, screenWidth / 5 - charTexture1.width / 2,
-                screenHeight / 2 - charTexture1.height / 2, WHITE);
-
-    if (b_singleplayer) {
-      currentPage = BEAT_SELECTION_SINGLE;
-    } else if (b_multiplayer) {
-      currentPage = BEAT_SELECTION_MULTI;
-    } else if (b_generator) {
-      currentPage = PAGE_GENERATOR;
-    } else if (b_quit) {
-      currentPage = QUIT_GAME;
-    }
-  }
-}
 
 void drawBeatSelectionPage(int is_multi) {
   char* title = "Select a Beatmap";
@@ -195,13 +113,13 @@ void drawBeatSelectionPage(int is_multi) {
       beatFile = beatmap_files.data[i];
       printf("Selected beatmap: %s\n", beatFile);
       // Here you would typically load the beatmap and transition to gameplay
-      currentPage = is_multi ? PAGE_MULTIPLAYER : PAGE_SINGLEPLAYER;
+      setCurrentPage(is_multi ? PAGE_MULTIPLAYER : PAGE_SINGLEPLAYER);
       beatFile = beatmap_files.data[i];
       printf("Selected beatmap: %s\n", beatFile);
       currentBeatmap = readBeatmap(beatFile);
       if (currentBeatmap == NULL) {
         printf("Failed to load beatmap: %s\n", beatFile);
-        currentPage = PAGE_MENU;  // go back to menu on failure
+        setCurrentPage(PAGE_MENU);  // go back to menu on failure
       }
       if (!is_multi) {
         prepare_game_singleplayer();
@@ -225,7 +143,7 @@ void prepare_game_singleplayer() {
   // Load the beatmap
   if (beatFile == NULL) {
     printf("No beatmap selected!\n");
-    currentPage = PAGE_MENU;
+    setCurrentPage(PAGE_MENU);
     return;
   }
 
